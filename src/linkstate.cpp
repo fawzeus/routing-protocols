9131ff2844f9c0899003549b5f6ceaa1e4f0215e
fawzirjili <rjilifawzi@gmail.com>
2021-11-19 22:08:10 +0100

    implementation of linkstate and reading messages

--- src/linkstate.cpp
+++ src/linkstate.cpp
@@ -4,11 +4,17 @@ using namespace std;
 
 int infinity = numeric_limits<int>::max();
 int graph [100][100];
+vector<string> path ;
+int dist[100][100]; // The output array.  dist[i] will hold the shortest
+    // distance from src to i
 
+bool sptSet[100]; // sptSet[i] will be true if vertex i is included in shortest
+    // path tree or shortest distance from src to i is finalized
 
+string paths[100][100];
 /******************************************/
 
-int minDistance(int dist[], bool sptSet[] ,int number_of_nodes)
+int minDistance(int src,int dist[100][100], bool sptSet[] ,int number_of_nodes)
 {
    
    int V = number_of_nodes+1;
@@ -16,8 +22,8 @@ int minDistance(int dist[], bool sptSet[] ,int number_of_nodes)
     int min = INT_MAX, min_index;
  
     for (int v = 0; v < V; v++)
-        if (sptSet[v] == false && dist[v] <= min)
-            min = dist[v], min_index = v;
+        if (sptSet[v] == false && dist[src][v] <= min)
+            min = dist[src][v], min_index = v;
  
     return min_index;
 }
@@ -36,30 +42,29 @@ void printSolution(int dist[] , int number_of_nodes)
 // for a graph represented using adjacency matrix representation
 void dijkstra(int src , int number_of_nodes)
 {
+    path.clear();
     int V = number_of_nodes+1;
-    vector<string> path ;
-    int dist[V]; // The output array.  dist[i] will hold the shortest
-    // distance from src to i
+    
  
-    bool sptSet[V]; // sptSet[i] will be true if vertex i is included in shortest
-    // path tree or shortest distance from src to i is finalized
+    
  
     // Initialize all distances as INFINITE and stpSet[] as false
     
     for (int i = 1; i < V; i++){
-        dist[i] = INT_MAX, sptSet[i] = false;
+        dist[src][i] = INT_MAX, sptSet[i] = false;
         path.push_back("");
+        paths[src][i]="";
     }
     
  
     // Distance of source vertex from itself is always 0
-    dist[src] = 0;
+    dist[src][src] = 0;
  
     // Find shortest path for all vertices
     for (int count = 0; count < V - 1; count++) {
         // Pick the minimum distance vertex from the set of vertices not
         // yet processed. u is always equal to src in the first iteration.
-        int u = minDistance(dist, sptSet, number_of_nodes);
+        int u = minDistance(src,dist, sptSet, number_of_nodes);
  
         // Mark the picked vertex as processed
         sptSet[u] = true;
@@ -70,22 +75,21 @@ void dijkstra(int src , int number_of_nodes)
             // Update dist[v] only if is not in sptSet, there is an edge from
             // u to v, and total weight of path from src to  v through u is
             // smaller than current value of dist[v]
-            if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX
-                && dist[u] + graph[u][v] < dist[v] ||(!sptSet[v] && graph[u][v] && dist[u] != INT_MAX
+            if (!sptSet[v] && graph[u][v] && dist[src][u] != INT_MAX
+                && dist[src][u] + graph[u][v] < dist[src][v] ||(!sptSet[v] && graph[u][v] && dist[src][u] != INT_MAX
                 && dist[u] + graph[u][v] == dist[v] && u < path[v-1].back() - 0 )){
-                dist[v] = dist[u] + graph[u][v];
-                cout<<"path u-1 "<<path[u-1]<<" u "<<u<<endl; 
+                dist[src][v] = dist[src][u] + graph[u][v];
+                //cout<<"path u-1 "<<path[u-1]<<" u "<<u<<endl; 
                 path[v-1]= path[u-1]+to_string(u);
+                paths[src][v]=paths[src][u]+to_string(u);
                 }
     }
     path[src-1]=to_string(src);
+    paths[src][src]=to_string(src);
     // print the constructed distance array
-    printSolution(dist,number_of_nodes);
+    //printSolution(dist,number_of_nodes);
     //print the paths to each node
-    puts("path\n\n");
-    for(int i=0;i<path.size();i++){
-        cout<<path[i]<<endl;
-    }
+    
 }
 
 /*******************************************/
@@ -114,15 +118,24 @@ void dijkstra(int src , int number_of_nodes)
     
 // }
 
+void linkstate(int number_of_nodes){
+    for (int i=1;i<=number_of_nodes;i++){
+        dijkstra(i,number_of_nodes);
+    }
+}
+
 int main(){
     
     
     int number_of_nodes = 0;
     int x,y,z;
-    
-    freopen("input.in","r",stdin);
+    FILE* messagefile;
+    ifstream topofile("topofile.txt");
+    //ifstream messagefile("messagefile.txt");
+    ifstream changesfile("changesfile.txt");
+    messagefile = fopen("messagefile.txt","r");
     //cout<<infinity;
-    while (cin>>x>>y>>z){
+    while (topofile>>x>>y>>z){
         // cout<<x<<" "<<y<<" "<<z<<endl;
         // vect.push_back(make_pair(x,make_pair(y,z)));
         graph[x][y]=z;
@@ -132,7 +145,16 @@ int main(){
         //cout<<"number of nodes is :"<<number_of_nodes<<endl;
     }
     
-        dijkstra(1,number_of_nodes);
+    int src,dest;
+    
+    while (fscanf(messagefile, "%d %d ", &src, &dest) != EOF){
+        //cout<<"from "<<src<<" to "<<dest<<endl;
+        linkstate(number_of_nodes);
+        printf("from %d to %d cost %d hops \n", src, dest, dist[src][dest]);
+        
+        
+    }
+    fseek(messagefile, 0, SEEK_SET);
     // puts("");
     // for (int i=0;i<vect.size();i++){
     //     cout<<vect[i].first<<" "<<vect[i].second.first<<" "<<vect[i].second.second<<endl;
